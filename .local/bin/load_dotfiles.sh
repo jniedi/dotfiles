3bc7901a9b931d4bb24fe65f6af0a35923d60385
jaron <jaron.niederer@proton.me>
2025-06-15 13:30:06 +0200

    add nvim autodownload plugins

--- .local/bin/load_dotfiles.sh
+++ .local/bin/load_dotfiles.sh
@@ -11,7 +11,14 @@ cp -r $HOME/dotfiles-tmp/.* ~  # Copy everything
 rm -r $HOME/dotfiles-tmp
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -k; # NOTE: -k option keeps .zshrc
 
+
+
+
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 config config status.showUntrackedFiles no
+
+
+echo "Setting up nvim..."
+/usr/bin/nvim --headless "+Lazy! update" +qa
 
 echo "Everything set!"

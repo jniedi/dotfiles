c35ae2902891abbcd0ca8a20caa96c8e2692dd62
jaron <jaron.niederer@proton.me>
2025-06-15 13:17:27 +0200

    fix load_dotfiles.sh

--- .local/bin/load_dotfiles.sh
+++ .local/bin/load_dotfiles.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-
 error() {
     printf "Error: $1\n" >&2; exit 1
 }
@@ -8,7 +5,7 @@ error() {
 [ -d ~/.dotfiles ] && error '~/.dotfiles already exists'
 
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/jniedi/dotfiles.git $HOME/dotfiles-tmp
-cp $HOME/dotfiles-tmp/.* ~  # Copy everything
+cp -r $HOME/dotfiles-tmp/.* ~  # Copy everything
 rm -r $HOME/dotfiles-tmp
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -k; # NOTE: -k option keeps .zshrc
 echo "Everything set!"
